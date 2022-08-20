08dd5fb77d4a392442e766e0a8319d3c8bf79ead
jimestir <jimmyestir@gmail.com>
2022-08-20 14:24:21 +0200

    Add inptu type range in functionality

--- code.js
+++ code.js
@@ -1,21 +1,25 @@
 const box = document.getElementById("box");
 
-function customBorderRadius(bordersRadius) {
+function customBordersRadius(bordersRadius) {
   const { topLeft, topRight, bottomLeft, bottomRight } = bordersRadius;
-  box.style.borderTopLeftRadius = `${topLeft}px`;
-  box.style.borderTopRightRadius = `${topRight}px`;
-  box.style.borderBottomRightRadius = `${bottomRight}px`;
-  box.style.borderBottomLeftRadius = `${bottomLeft}px`;
+  box.style.borderTopLeftRadius = `${topLeft[0]}% ${topLeft[1]}%`;
+  box.style.borderTopRightRadius = `${topRight[0]}% ${topRight[1]}%`;
+  box.style.borderBottomRightRadius = `${bottomRight[0]}% ${bottomRight[1]}%`;
+  box.style.borderBottomLeftRadius = `${bottomLeft[0]}% ${bottomLeft[1]}%`;
 }
 
 function handleClick(e) {
   e.preventDefault();
+  const rangeTop = document.getElementById("top").value;
+  const rangeRight = document.getElementById("right").value;
+  const rangeBottom = document.getElementById("bottom").value;
+  const rangeLeft = document.getElementById("left").value;
 
   const bordersRadius = {
-    topLeft: document.getElementById("top-left").value,
-    topRight: document.getElementById("top-right").value,
-    bottomRight: document.getElementById("bottom-right").value,
-    bottomLeft: document.getElementById("bottom-left").value,
+    topLeft: [rangeTop, 100 - rangeLeft],
+    topRight: [100 - rangeTop, rangeRight],
+    bottomRight: [100 - rangeBottom, 100 - rangeRight],
+    bottomLeft: [rangeBottom, rangeLeft],
   };
-  customBorderRadius(bordersRadius);
+  customBordersRadius(bordersRadius);
 }
