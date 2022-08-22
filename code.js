e9470033f876b28c5d820be0dacbb4439b9468a5
jimestir <jimmyestir@gmail.com>
2022-08-22 19:39:32 +0200

    Add function copyClipboard

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 const box = document.getElementById("box");
-const btnCopy = document.querySelector('.btn-copy')
+const btnCopy = document.querySelector(".btn-copy");
 const printElement = document.querySelector(".print-element");
 
 function customBordersRadius(bordersRadius) {
@@ -14,13 +14,22 @@ function writeToDOM(element, content) {
   element.innerHTML = `border-radius: ${content};`;
 }
 
+function copyClipboard() {
+  let aux = document.createElement("input");
+  aux.setAttribute("value", printElement.innerHTML);
+  document.body.appendChild(aux);
+  aux.select();
+  document.execCommand("copy");
+  document.body.removeChild(aux);
+  alert("Copied!!");
+}
+
 function handleClick(e) {
   e.preventDefault();
   const rangeTop = document.getElementById("top").value;
   const rangeRight = document.getElementById("right").value;
   const rangeBottom = document.getElementById("bottom").value;
   const rangeLeft = document.getElementById("left").value;
-  console.log({ rangeTop, rangeRight, rangeBottom, rangeLeft });
   const bordersRadius = {
     topLeft: [rangeTop, 100 - rangeLeft],
     topRight: [100 - rangeTop, rangeRight],
